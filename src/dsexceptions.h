a21a96b77f7255bd43f3bf5934159352f2ded611
lukejuusola <luke.juusola@gmail.com>
2016-02-18 02:22:00 -0800

    added header guards

--- src/dsexceptions.h
+++ src/dsexceptions.h
@@ -1,5 +1,10 @@
+#ifndef _EXCEPTIONS
+#define _EXCEPTIONS
+
 #include <exception>
 
 class EmptyPopException : public std::exception {};
 class NotFoundException : public std::exception {};
 class OutOfBoundsException : public std::exception {};
+
+#endif
